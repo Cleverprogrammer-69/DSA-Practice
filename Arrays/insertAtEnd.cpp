ae4f713784adb312d5a61c485283ad1747481439
Cleverprogrammer-69 <hafizabdullah376@gmail.com>
2024-06-04 17:03:56 +0500

    arrayPush is created

---
+++ Arrays/insertAtEnd.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int arrayPush(int arr[], int &size, int element){
+    arr[size] = element;
+    return size++;
+}
+void display(int arr[], int size){
+    for(int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int arr[100] = {2,4,1,-2,100};
+    int size = 5, element = 101;
+    arrayPush(arr, size, element);
+    cout<<size<<endl;
+    display(arr, size);
+    return 0;
+}
